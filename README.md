0f8108ea4a160023da35381227694879dece0b05
Leandro Oliveira de Moraes <lomoraes@fazenda.rj.gov.br>
2022-01-04 03:29:09 -0300

    Projeto base com porta configurada para 8081 no application.properties

---
+++ README.md
@@ -0,0 +1,3 @@
+PROJETO BASE DE EXEMPLO USANDO AS MELHORES PRATICAS DE DESENVOLVIMENTO
+======================================================================
+Springboot, Hibernate, Java 11, SGDB Oracle
---
+++ src/test/java/com/github/leandromoraesrj/sincadbackend/SincadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leandromoraesrj.sincadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SincadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
