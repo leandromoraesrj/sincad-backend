52b2e472ba06ab126bcd625eb0fb9388563304ee
Leandro Oliveira de Moraes <lomoraes@fazenda.rj.gov.br>
2022-01-05 02:30:44 -0300

    Adicionado os pacotes padronizados e profiles

--- src/main/java/com/github/leandromoraesrj/sincadbackend/SincadBackendApplication.java
+++ src/main/java/com/github/leandromoraesrj/sincadbackend/SincadBackendApplication.java
@@ -1,13 +1,32 @@
 package com.github.leandromoraesrj.sincadbackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SincadBackendApplication {
+	//Para Injetar um parâmetro de aplication.properties
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${server.servlet.context-path}")
+	private String applicationConext;
+	
+	@Value("${spring.profiles.active}")
+	private String applicationProfile;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SincadBackendApplication.class, args);
 	}
-
+	
+	//Para obter informações da aplicação, por exemplo, o contexto 
+	@RequestMapping(value = "/teste", method = RequestMethod.GET)
+	public String teste() {
+		return "Nome da Aplicação: " + applicationName + "\n" + "Contexto da Aplicação: " + applicationConext + "\n" + "Ambiente da Aplicação: " + applicationProfile;
+	}
 }
---
+++ src/main/java/com/github/leandromoraesrj/sincadbackend/configurations/testConfiguration.java
@@ -0,0 +1,32 @@
+package com.github.leandromoraesrj.sincadbackend.configurations;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile(value = "test")
+public class testConfiguration {
+	//Para Injetar um parâmetro de aplication.properties
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${server.servlet.context-path}")
+	private String applicationConext;
+	
+	@Value("${spring.profiles.active}")
+	private String applicationProfile;
+		
+	//Para o Logger exibir mensagem no console
+	private static Logger logger = LoggerFactory.getLogger(testConfiguration.class);
+	
+	@Bean
+	public void teste() {
+		logger.info("Nome da Aplicação: " + applicationName);
+		logger.info("Contexto da Aplicação: " + applicationConext);
+		logger.info("Ambiente da Aplicação: " + applicationProfile);
+	}
+}
